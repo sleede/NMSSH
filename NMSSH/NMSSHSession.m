75fd599cea2a58b748455a9648a9eef24a639057
Du Peng <gnepud@gmail.com>
2026-03-14 18:10:27 +0100

    feat: Add more KEX algorithms (curve25519, ecdh-sha2)

--- NMSSH/NMSSHSession.m
+++ NMSSH/NMSSHSession.m
@@ -334,7 +334,7 @@ static void nmssh_trace_callback(LIBSSH2_SESSION *session,
     }
 
     // Set algorithm preferences for compatibility
-    libssh2_session_method_pref(self.session, LIBSSH2_METHOD_KEX, "curve25519-sha256,diffie-hellman-group14-sha256,diffie-hellman-group16-sha512,diffie-hellman-group18-sha512,diffie-hellman-group14-sha1");
+    libssh2_session_method_pref(self.session, LIBSSH2_METHOD_KEX, "curve25519-sha256,curve25519-sha256@libssh.org,ecdh-sha2-nistp256,ecdh-sha2-nistp384,ecdh-sha2-nistp521,diffie-hellman-group-exchange-sha256,diffie-hellman-group14-sha256,diffie-hellman-group16-sha512,diffie-hellman-group18-sha512,diffie-hellman-group14-sha1");
     libssh2_session_method_pref(self.session, LIBSSH2_METHOD_HOSTKEY, "ssh-ed25519,rsa-sha2-512,rsa-sha2-256,ssh-rsa,ecdsa-sha2-nistp256,ecdsa-sha2-nistp384,ecdsa-sha2-nistp521");
     libssh2_session_method_pref(self.session, LIBSSH2_METHOD_CRYPT_CS, "aes128-ctr,aes192-ctr,aes256-ctr,aes128-cbc,aes192-cbc,aes256-cbc");
     libssh2_session_method_pref(self.session, LIBSSH2_METHOD_CRYPT_SC, "aes128-ctr,aes192-ctr,aes256-ctr,aes128-cbc,aes192-cbc,aes256-cbc");
